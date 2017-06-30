c8c369d52c77971ab598d45deca0229ea83f676b
GUILDAM <bruno.heidorn@hotmail.com>
2017-06-30 19:22:42 -0300

    Ajustes finais

--- controller/MainController.go
+++ controller/MainController.go
@@ -4,10 +4,11 @@ import (
 	"net/http"
 	"regexp"
 	"html/template"
+	"os"
 )
 
 
-var source = ""// os.Getenv("GOPATH") + "/src/github.com/guildam/ep/"
+var source = os.Getenv("GOPATH") + "/src/github.com/guildam/ep/"
 var templates = template.Must(template.ParseFiles(source+"home.html", source+"template/menu.html",
 	source+"template/edit.html", source+"template/view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$|^/$")
